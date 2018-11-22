d9169b17b637e7f3e3f0ed27444bbc41fb7549e1
shenxiaoming77 <526633123@qq.com>
2018-11-22 23:20:32 +0800

    'mmmmmmmmmmmmmmmmmmm'

--- temp.py
+++ temp.py
@@ -1,386 +1,162 @@
-import numpy as np
-import re
-import time
-import datetime
-import pandas as pd
-import decimal
-from dateutil.relativedelta import relativedelta
-
-city_0=['北京','上海','广州','深圳']
-
-city_01=['成都市','杭州市','重庆市','武汉市','苏州市','西安市','天津市','南京市','郑州市','长沙市','沈阳市','青岛市','宁波市','东莞市','无锡市']
-
-city_1=['江汉','济南','厦门','哈尔滨','长春','福州','石家庄','佛山','烟台','合肥','昆明','唐山','济南'
-       ,'大连','厦门','哈尔滨','长春','福州','石家庄'
-       ,'佛山','烟台','太原','合肥','南昌','南宁','昆明','温州','淄博','唐山']
-
-city_2=[ '省直','延吉','涪陵','珲春','舟山','淮阴', '阜新','襄樊','河源','梧州','三亚','延边','衡阳','梅州','乌鲁木齐','贵阳','海口','兰州','银川','西宁','呼和浩特','泉州','包头','南通','大庆','徐州','潍坊','常州','鄂尔多斯'
-    ,'绍兴','济宁','盐城','邯郸','临沂','洛阳','东营','扬州','台州','嘉兴','沧州','榆林','泰州','镇江','昆山','江阴','张家港'
-    ,'义乌','金华','保定','吉林','鞍山','泰安','宜昌','襄阳','中山','惠州','南阳','威海','德州','岳阳','聊城','常德','漳州'
-    ,'滨州','茂名','淮安','江门','芜湖','湛江','廊坊','菏泽','柳州','宝鸡','珠海','绵阳']
-
-city_3=['德令哈','仙桃','康定','伊犁', '中卫', '黔江','万州','思茅','迪庆', '新余','怒江','临沧','双鸭山','甘孜','铜仁','商洛','景德镇','漯河','潮州','白银','湘西', '西双版纳','葫芦岛','抚州','黄山','白山','淮南', '恩施','眉山','平凉','河池','钦州','巢湖', '宣城','亳州','海北','株洲','枣庄','许昌','通辽','湖州','新乡','咸阳','松原','连云港','安阳','周口','焦作','赤峰','邢台','郴州','宿迁'
-    ,'赣州','平顶山','桂林','肇庆','曲靖','九江','商丘','汕头','信阳','驻马店','营口','揭阳','龙岩','安庆','日照','遵义'
-    ,'三明','呼伦贝尔','长治','湘潭','德阳','南充','乐山','达州','盘锦','延安','上饶','锦州','宜春','宜宾','张家口','马鞍山'
-    ,'吕梁','抚顺','临汾','渭南','开封','莆田','荆州','黄冈','四平','承德','齐齐哈尔','三门峡','秦皇岛','本溪','玉林','孝感'
-    ,'牡丹江','荆门','宁德','运城','绥化','永州','怀化','黄石','泸州','清远','邵阳','衡水','益阳','丹东','铁岭','晋城','朔州'
-    ,'吉安','娄底','玉溪','辽阳','南平','濮阳','晋中','资阳','都江堰','攀枝花','衢州','内江','滁州','阜阳','十堰','大同'
-    ,'朝阳','六安','宿州','通化','蚌埠','韶关','丽水','自贡','阳江','毕节']
-
-city_4=['奎屯','海晏', '梅河', '锡', '林芝', '景洪','博尔','黄南', '格尔','济源','兴义','凯里', '贵池','都匀','吉首','汉中', '来宾市','甘南','德宏','大兴安岭','海南','玉树', '甘南'
-    , '陇南','海西', '阳泉','铜陵', '金昌','凉山','七台河', '鄂州','莱芜','伊春', '铜川','临夏','海东'
-    ,'崇左','辽源','忻州','汕尾','固原','安康','定西','庆阳','楚雄','池州','鸡西','贺州','防城港','萍乡'
-    ,'红河','随州','咸宁','文山', '黔东','张家界','达川','安顺','白城','黔南','贵港', '阿坝','云浮', '鹤壁'
-    ,'佳木斯','黔西','六盘水','黑河','鹤岗','淮北','鹰潭','拉萨','克拉玛依','库尔勒','昌吉','哈密','伊宁','喀什','阿克苏','石河子','晋江','增城','诸暨','丹阳','玉环','常熟'
-    ,'崇明','余姚','奉化','海宁','浏阳市','大理','丽江','普洱','保山','昭通','西昌','雅安','广安','广元','巴中','遂宁'
-    ,'天水','酒泉','嘉峪关','武威','张掖','石嘴山','吴忠','北海','百色','虎门镇','长安镇','鳌江-龙港镇']
+# -*- coding:utf-8 -*-
 
 
-def ConvertCity(x):  #一线城市值越小
+from splinter.driver.webdriver.chrome import Options, Chrome
+from splinter.browser import Browser
+from contextlib import closing
+import requests, json, time, re, os, sys, time
+from bs4 import BeautifulSoup
 
-    if x in city_0:
-       return 0
-    elif x in city_01:
-       return 1
-    elif x in city_1:
-       return 1
-    elif x in city_2:
-       return 2
-    elif x in city_3:
-       return 3
-    elif x in city_4:
-       return 3
-    elif x in " ".join(city_0):
-       return 0
-    elif x in " ".join(city_01):
-       return 1
-    elif x in " ".join(city_1):
-       return 1
-    elif x in " ".join(city_2):
-       return 2
-    elif x in " ".join(city_3):
-       return 3
-    else:
-       return 3
+class DouYin(object):
+    def __init__(self, width = 500, height = 300):
+        """
+        抖音App视频下载
+        """
+        # 无头浏览器
+        chrome_options = Options()
+        chrome_options.add_argument('user-agent="Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36"')
+        self.driver = Browser(driver_name='chrome', executable_path='D:/chromedriver', options=chrome_options, headless=True)
 
-def ConvertProvince(x):   #x表示违约率,x越小，该省赋予的得分越大
-        if x< 10: #x<10
-            return 3
-        elif x<15:   #10<=x<15
-            return 2
-        elif x<20:  #15<=x<20
-            return 1
+    def get_video_urls(self, user_id):
+        """
+        获得视频播放地址
+        Parameters:
+           user_id：查询的用户ID
+       Returns:
+            video_names: 视频名字列表
+           video_urls: 视频链接列表
+            nickname: 用户昵称
+        """
+        video_names = []
+        video_urls = []
+        unique_id = ''
+        while unique_id != user_id:
+            search_url = 'https://api.amemv.com/aweme/v1/discover/search/?cursor=0&keyword=%s&count=10&type=1&retry_type=no_retry&iid=17900846586&device_id' \
+                         '=34692364855&ac=wifi&channel=xiaomi&aid=1128&app_name=aweme&version_code=162&version_name=1.6.2&device_platform=android&ssmix=a&device_type=MI+5&device_brand=Xiaomi&os_api=24&os_version=7.0&uuid=861945034132187&openudid=dc451556fc0eeadb&manifest_version_code=162&resolution=1080*1920&dpi=480&update_version_code=1622' % user_id
+            req = requests.get(url = search_url, verify = False)
+            html = json.loads(req.text)
+            aweme_count = html['user_list'][0]['user_info']['aweme_count']
+            uid = html['user_list'][0]['user_info']['uid']
+            nickname = html['user_list'][0]['user_info']['nickname']
+            unique_id = html['user_list'][0]['user_info']['unique_id']
+        user_url = 'https://www.douyin.com/aweme/v1/aweme/post/?user_id=%s&max_cursor=0&count=%s' % (uid, aweme_count)
+        req = requests.get(url = user_url, verify = False)
+        html = json.loads(req.text)
+        i = 1
+        for each in html['aweme_list']:
+            share_desc = each['share_info']['share_desc']
+            if '抖音-原创音乐短视频社区' == share_desc:
+                video_names.append(str(i) + '.mp4')
+                i += 1
+            else:
+                video_names.append(share_desc + '.mp4')
+            video_urls.append(each['share_info']['share_url'])
+
+        return video_names, video_urls, nickname
+
+    def get_download_url(self, video_url):
+        """
+        获得带水印的视频播放地址
+        Parameters:
+            video_url：带水印的视频播放地址
+        Returns:
+            download_url: 带水印的视频下载地址
+        """
+        req = requests.get(url = video_url, verify = False)
+        bf = BeautifulSoup(req.text, 'lxml')
+        script = bf.find_all('script')[-1]
+        video_url_js = re.findall('var data = \[(.+)\];', str(script))[0]
+        video_html = json.loads(video_url_js)
+        download_url = video_html['video']['play_addr']['url_list'][0]
+        return download_url
+
+    def video_downloader(self, video_url, video_name, watermark_flag=True):
+        """
+        视频下载
+        Parameters:
+            video_url: 带水印的视频地址
+            video_name: 视频名
+            watermark_flag: 是否下载不带水印的视频
+        Returns:
+            无
+        """
+        size = 0
+        if watermark_flag == True:
+            video_url = self.remove_watermark(video_url)
         else:
-            return 0   #>=20
+            video_url = self.get_download_url(video_url)
+        with closing(requests.get(video_url, stream=True, verify = False)) as response:
+            chunk_size = 1024
+            content_size = int(response.headers['content-length'])
+            if response.status_code == 200:
+                sys.stdout.write('  [文件大小]:%0.2f MB\n' % (content_size / chunk_size / 1024))
 
-def ConvertDateDiff(x):
-    if x==0 :
-        return 0
-    elif x==1:
-        return 1
-    elif x==2:
-        return 1
-    elif x==3:
-        return 2
-    elif x==4:
-        return 2
-    elif x==5:
-        return 2
-    elif x==6:
-        return 3
-    else:
-        return 4
-#
-# def ConvertAge(x):
-#     if 18<=x<=40:
-#         return 0
-#     elif 40<x<50:
-#         return 1
-#     elif 50<=x<=60:
-#         return 2
+                with open(video_name, "wb") as file:
+                    for data in response.iter_content(chunk_size = chunk_size):
+                        file.write(data)
+                        size += len(data)
+                        file.flush()
 
-def ConvertAge(x):
-    if 18<=x<20:
-        return 0
-    elif 20<=x<=40:
-        return 1
-    elif 40<x<50:
-        return 2
-    elif 50<=x<=55:
-        return 3
-    elif 55<x<=60:
-        return 4
-
-def ConvertApplyDateHour(x):
-    if 0<=x<=6:
-        return 0
-    elif 6<x<=10:
-        return 1
-    elif 10<x<=20:
-        return 2
-    elif 20<x<=23:
-        return 3
-
-def ConvertregisterDateHour(x):
-    if 0<=x<2 or 20<=x<=23:
-        return 0
-    elif 2<=x<=6:
-        return 1
-    elif 6<x<=10:
-        return 2
-    elif 10<x<=19:
-        return 3
-
-#百融信用分
-#[300,500) 高风险，建议拒绝
-#[500,550) 中风险，建议关注
-#[550,1000] 低风险风险，建议通过
-
-# def ConvertBrScore(x):
-#     if x=="Unknown":
-#         return 0
-#     elif 300<=x<500:
-#         return 0
-#     elif 500<=x<550:
-#         return 1
-#     elif 550<=x<=1000:
-#         return 2
-#     else:
-#         return 0
-
-def ConvertBrScore(x):
-    if x=="Unknown":
-        return 0
-    elif 300<=x<500:
-        return 1
-    elif 500<=x<600:
-        return 2
-    elif 600<=x<=700:
-        return 3
-    elif 700<=x<=1000:
-        return 4
-
-#芝麻信用分
-#第一级：300分~500分
-#第二级：500分~600分
-#第三级：600分~700分
-#第四级：700分~800分
-#第五级：800分~950分
-# def ConvertZhimaScore(x):
-#     if x=="Unknown":
-#         return 2
-#     elif 300<=x<500:
-#         return 0
-#     elif 500<=x<600:
-#         return 1
-#     elif 600<=x<=700:
-#         return 2
-#     elif 700<=x<=800:
-#         return 3
-#     elif 800<=x<=950:
-#         return 4
-#     else:
-#         return 2
-
-def ConvertZhimaScore(x):
-
-    if 300<=x<600:
-        return 0
-    elif 600<=x<650:
-        return 1
-    elif 650<=x<=700:
-        return 2
-    elif 700<=x<=950:
-        return 3
-
-def ConvertCompanyCollegeLength(x):
-    if x=="Unknown":
-         return 0
-    elif 0<=x<16:
-        return 1
-    elif 16<=x<=23:
-        return 2
-
-#对于连续变量用badrate编码，其实肯定能分组分得更好，但是模型效果不一定好
-def ConvertM1OverdueCount(x):
-    if x>0:
-        return 1
-    else:
-        return 0
-
-def ConvertM3RepayremindCount(x):
-    if x<=1:
-        return 0
-    elif 2<=x<=4:
-        return 1
-    elif x>4:
-        return 2
-
-def ConvertOccupationGender(x):
-    if x=='Unknown_0':
-        return 'Unknown'
-    elif x=='Unknown_1':
-        return 'Unknown'
-    else:
-        return x
-
-def  areaVarMap(data,tag,var):
-
-        provinces = data[var].unique() # 省类别
-        provinces = [x for x in provinces if str(x) != 'nan']
-        #print(provinces)
-        neg = data[data[tag]==1]
-
-        #贷款逾期占比
-        negSer = pd.value_counts(neg[var],sort=False)
-
-        posprovList=negSer.index
-
-        for i in provinces:
-            if not (i  in  posprovList):
-                negSer[i]=0
-
-        provinceSer =pd.value_counts(data[var],sort=False)
-
-        pct= negSer.sort_index()/provinceSer.sort_index()*100
-        # print(pct)
-        pct =pct.map(lambda x: ConvertProvince(x))
-        return  pct
-
-def  badRateMap(df,tag,col):
-
-    total = df.groupby([col])[tag].count()
-    total = pd.DataFrame({'total': total})
-    bad = df.groupby([col])[tag].sum()
-    bad = pd.DataFrame({'bad': bad})
-
-    regroup = total.merge(bad, left_index=True, right_index=True, how='left')
-    regroup.reset_index(level=0, inplace=True)
-    regroup[col] = regroup.apply(lambda x: round(x.bad * 1.0 / x.total*100,1), axis=1)
-    # print(regroup[col+'_'+'bad_rate'])
-    return regroup[col]
+                        sys.stdout.write('  [下载进度]:%.2f%%' % float(size / content_size * 100) + '\r')
+                        sys.stdout.flush()
 
 
+    def remove_watermark(self, video_url):
+        """
+        获得无水印的视频播放地址
+        Parameters:
+            video_url: 带水印的视频地址
+        Returns:
+            无水印的视频下载地址
+        """
+        self.driver.visit('http://douyin.iiilab.com/')
+        self.driver.find_by_tag('input').fill(video_url)
+        self.driver.find_by_xpath('//button[@class="btn btn-default"]').click()
+        html = self.driver.find_by_xpath('//div[@class="thumbnail"]/div/p')[0].html
+        bf = BeautifulSoup(html, 'lxml')
+        return bf.find('a').get('href')
+
+    def run(self):
+        """
+        运行函数
+        Parameters:
+            None
+        Returns:
+            None
+       """
+        self.hello()
+        user_id = input('请输入ID(例如40103580):')
+        video_names, video_urls, nickname = self.get_video_urls(user_id)
+        if nickname not in os.listdir():
+            os.mkdir(nickname)
+        print('视频下载中:共有%d个作品!\n' % len(video_urls))
+        for num in range(len(video_urls)):
+            print('  解析第%d个视频链接 [%s] 中，请稍后!\n' % (num+1, video_urls[num]))
+            if '\\' in video_names[num]:
+                video_name = video_names[num].replace('\\', '')
+            elif '/' in video_names[num]:
+                video_name = video_names[num].replace('/', '')
+            else:
+                video_name = video_names[num]
+            self.video_downloader(video_urls[num], os.path.join(nickname, video_name))
+            print('\n')
+
+        print('下载完成!')
+
+    def hello(self):
+        """
+        打印欢迎界面
+        Parameters:
+            None
+        Returns:
+            None
+        """
+        print('*' * 100)
+        print('\t\t\t\t抖音App视频下载小助手')
+        print('\t\t作者:Python学习交流群：125240963')
+        print('*' * 100)
 
 
-def negToZero(x):
-    if x<0:
-        return 0
-    else :
-        return x
-
-'''
-
-
-Frequency count for variable network_len
-(24,+)     8753
-Unknown    6698
-(12,24]    1332
-(6,12]      410
-6           350
-未查得         255    #将未查得映射为UnKnown
-(0,6]        14
-(3,6]         9
-
--1   15.87804
- 0   10.99196
- 1   24.87805
- 2   24.32432
- 3   18.97635
-
- 0   15.62927
-1   19.88566
-'''
-mapping_dict = {
-    "network_len": {
-        "(24,+)": 3,
-        "(12,24]": 2,
-        "(6,12]": 2,
-        6: 1,
-        "(3,6]": 1,
-        "(0,6]": 1,
-        "Unknown": 4,
-        "未查得": 0,
-
-    },
-    # "network_len": {
-    #     "(24,+)": 4,
-    #     "(12,24]": 3,
-    #     "(6,12]": 2,
-    #     6: 1,
-    #     "(3,6]": 1,
-    #     "(0,6]": 1,
-    #     "Unknown": 0,
-    #     "未查得": 0,
-    #
-    # },
-    "gender":{
-        "男": 1,
-        "女": 0,
-    },
-    "job_level":{
-         # '主任/主管/组长/初级管理' :'主任/主管/组长/初级管理',
-         '主管' :'主任/主管/组长/初级管理',
-         '总经理' :'总监/总经理/高管',
-         # '总监/总经理/高管' :'总监/总经理/高管',
-         '总监':'总监/总经理/高管',
-         # '经理/中级管理' :'经理/中级管理',
-         '经理':'经理/中级管理',
-         # '法人/老板':'法人/老板',
-          '实习生':'学生',
-         #'学生':'学生',
-         # 'Unknown':'Unknown',
-         '司机' :'普通员工',
-         '服务员':'普通员工',
-         '厨师':'普通员工',
-         '保安':'普通员工',
-         '一般工人':'普通员工',
-         # '普通员工':'普通员工',
-         '技术工人':'普通员工',
-         '行政/人力资源' :'行政/人力资源/财会人员/助理',
-         '财会人员':'行政/人力资源/财会人员/助理',
-         '助理':'行政/人力资源/财会人员/助理',
-         '专业技术人员':'专业技术人员/设计师/工程师',
-         '设计师':'专业技术人员/设计师/工程师',
-         '工程师':'专业技术人员/设计师/工程师',
-         '医护人员': '医护人员/教师/军人/科级以上',
-         '科级以上': '医护人员/教师/军人/科级以上',
-         '教师': '医护人员/教师/军人/科级以上',
-         '军人': '医护人员/教师/军人/科级以上',
-         # '销售人员': '销售人员',
-         '一般科员' :'一般科员/专员',
-         '专员' :'一般科员/专员',
-         # '农林牧渔养殖人员' :  '农林牧渔养殖人员',
-         # '演员/运动员':'演员/运动员',
-         # '其他' :'其他'
-    }
-
-
-}
-
-
-
-
-def MonthGap(earlyDate, lateDate):
-    if lateDate > earlyDate:
-        gap = relativedelta(lateDate,earlyDate)  #计算时间跨度
-        yr = gap.years
-        mth = gap.months
-        return yr*12+mth
-    else:
-        return 0
-
-
-def MakeupMissing(x):
-    if np.isnan(x):
-        return -1
-    else:
-        return x
-
-
-def ModifyDf(x, new_value):
-    if np.isnan(x):
-        return new_value
-    else:
-        return x
-
+if __name__ == '__main__':
+    douyin = DouYin()
+    douyin.run()
--- test.py
+++ test.py
@@ -1,201 +1,76 @@
 #coding=utf-8
+import urllib.request as urlrequest
+from bs4 import BeautifulSoup
+import re
+import csv, codecs
 
-# dicts = {'a':'ff','b':'bb','1':'11','a':'aa',}
-# print(list(dicts.keys()))
-# print(list(dicts.values()))
+top250_url = 'https://movie.douban.com/top250?start={}&filter='
+movie_name = '名称'
+movie_assess = '评价人数'
+movie_score = '评分'
+movie_url = '链接'
+movie_intro = '介绍'
+movie_num = 0
 
-from  settings import  *
-import  pickle
-import  pandas as pd
-import  numpy as np
-import  seaborn as sns
-import  matplotlib.pyplot as plt
-from  util.scorecard_functions import *
+# print('{} {} {} {} {}'.format(movie_name,movie_assess,movie_score,movie_url,movie_intro))
+with open('top250_movie.csv', 'w', encoding='utf8') as outputfile:
+    # outputfile.write(codecs.BOM_UTF8)
+    writer = csv.writer(outputfile)
+    # writer.writerow(["movie_num","movie_name","movie_assess","movie_score","movie_url","movie_intro"])
+    outputfile.write(
+        "movie_num#movie_name#movie_year#movie_country#movie_type#movie_director#movie_assess#movie_score#movie_url#movie_intro\n")
+    for list in range(10):
+        movies_content = urlrequest.urlopen(top250_url.format(list * 25)).read()
+        movies_html = movies_content.decode('utf8')
+        moviessoup = BeautifulSoup(movies_html, 'html.parser')
+        all_list = moviessoup.find_all(class_='item')
+        # print(all_list)
+        for item in all_list:
+            item_data = item.find(class_='pic')
+            movie_url = item_data.find('a')['href']
+            movie_name = item_data.find('img')['alt']
+            item_info = item.find(class_='star')
+            info = item.find('div', attrs={'class': 'star'})
+            # find_all 将star标签中的所有span 存入一个列表中
+            movie_assess = info.find_all('span')[3].get_text()[:-3]
+            movie_score = item_info.find('span', attrs={'class': 'rating_num'}).get_text()
+            try:
+                movie_intro = item.find(class_='quote').find(class_='inq').get_text()
+            except Exception as e:
+                movie_intro = 'None'
 
-# df1 = pd.read_excel(ROOT_DIR + 'settings/1.xls', encoding='gbk')
-#
-#
-# df2 = pd.read_excel(ROOT_DIR + 'settings/2.xls', encoding='gbk')
-#
-#
-# df3 = pd.read_excel(ROOT_DIR + 'settings/3.xls', encoding='gbk')
-#
-#
-# df4 = pd.read_excel(ROOT_DIR + 'settings/4.xls', encoding='gbk')
-#
-#
-# df5 = pd.read_excel(ROOT_DIR + 'settings/5.xls', encoding='gbk')
-#
-# df6 = pd.read_excel(ROOT_DIR + 'settings/6.xls', encoding='gbk')
-#
-# city_dict = {}
-# city_dict['一线'] = list(df1['一线'])
-# city_dict['新一线'] = list(df2['新一线'])
-# city_dict['二线'] = list(df3['二线']) + ['省直']
-# city_dict['三线'] = list(df4['三线']) + ['淮安']
-# city_dict['四线'] = list(df5['四线'])
-# city_dict['五线'] = list(df6['五线']) + ['达州']
-#
-# print(city_dict)
-#
-# with open(ROOT_DIR + 'settings/city_classification.pkl', 'wb') as file:
-#     pickle.dump(city_dict, file)
-#
-# with open(ROOT_DIR + 'settings/city_classification.pkl', 'rb') as file:
-#     city_dict = pickle.load(file)
-#     print(city_dict)
+            movie_num = movie_num + 1
+            # print(movie_assess)
+            # print(item_assissent)
+            # item_assisent = item_data.find(name='span',attrs={'property':'v:average'})
 
-# flights_long = sns.load_dataset("flights")
-# print(flights_long)
-#
-# flights = flights_long.pivot("month", "year", "passengers")
-#
-# print(flights)
+            # 抓取电影上映年份、 导演、主演等信息
+            movie_actor_infos_html = item.find(class_='bd')
+            # strip() 方法用于移除字符串头尾指定的字符（默认为空格）
+            movie_actor_infos = movie_actor_infos_html.find('p').get_text().strip().split('\n')
+            actor_infos1 = movie_actor_infos[0].split('\xa0\xa0\xa0')
+            movie_director = actor_infos1[0][3:]
+            # print(movie_director)
+            movie_role = movie_actor_infos[1]
 
-# list_2 = []
-# list1 = [1, 2, 3]
-# list2 = [4, 5, 6]
-# list3 = [7, 8, 9]
-#
-# list_2.append(list1)
-# list_2.append(list2)
-# list_2.append(list3)
-#
-# mat = np.array(list_2)
-#
-# print(mat)
-#
-# print(mat.shape)
-#
-# user_info_df = pd.read_excel(ROOT_DIR + 'user_info.xlsx', encoding='utf-8')
-# numerical_df = user_info_df[numericalFeatures]
-# numerical_df.fillna(-1, inplace = True)
-#
-# corr_mat = numerical_df.corr()
-#
-# print(corr_mat)
-#
-# print(corr_mat.index)
+            movie_year_area = movie_actor_infos[1].lstrip().split('\xa0/\xa0')
+            movie_year = movie_year_area[0]
+            # print(movie_year)
+            movie_country = movie_year_area[1]
+            # print(movie_country)
 
-#user_info_df = pd.read_excel(ROOT_DIR + 'user_info.xlsx', encoding='utf-8')
-# numerical_df = user_info_df[numericalFeatures + ['user_id']]
-#
-# null_df = numerical_df[numerical_df['zhima_score'].isnull()]
-#
-# notnull_df = numerical_df[numerical_df['zhima_score'].notnull()]
-#
-# print(notnull_df)
+            movie_type = movie_year_area[2]
+            # print(movie_type)
 
-# joblevel_df = user_info_df[['job_level', 'loan_status']]
-# joblevel_df.fillna('未知', inplace = True)
-# jobs = set(joblevel_df['job_level'])
-# print(jobs)
+            # print('{} {} {} {} {} {} {} {} {} {}'.format(movie_num,movie_name,movie_year,movie_country,movie_type,movie_director,movie_assess,movie_score,movie_url,movie_intro))
 
+            # writer.writerow([movie_num,movie_name,movie_assess,movie_score,movie_url,movie_intro])
 
-# np.random.seed(2)  #设置随机种子
-# df = pd.DataFrame(np.random.rand(5,4),
-#                   columns=['A', 'B', 'C', 'D'])
-#
-# plt.boxplot(x=df.values,labels=df.columns,whis=1.5)
-#
-# plt.show()
+            if movie_type == '':
+                movie_type = 'NULL'
 
-# def list_generator(mean, dis, number):  # 封装一下这个函数，用来后面生成数据
-#     return np.random.normal(mean, dis * dis, number)  # normal分布，输入的参数是均值、标准差以及生成的数量
+            outputfile.write(
+                '{}#{}#{}#{}#{}#{}#{}#{}#{}#{}\n'.format(movie_num, movie_name, movie_year, movie_country, movie_type,
+                                                         movie_director, movie_assess, movie_score, movie_url,
+                                                         movie_intro))
 
-
-# # 我们生成四组数据用来做实验，数据量分别为70-100
-# list1 = list_generator(0.8531, 0.0956, 70)
-# list2 = list_generator(0.8631, 0.0656, 80)
-# list3 = list_generator(0.8731, 0.1056, 90)
-# list4 = list_generator(0.8831, 0.0756, 100)
-# s1 = pd.Series(np.array(list1))
-# s2 = pd.Series(np.array(list2))
-# s3 = pd.Series(np.array(list3))
-# s4 = pd.Series(np.array(list4))
-# # 把四个list导入到pandas的数据结构中，dataframe
-# data = pd.DataFrame({"1": s1, "2": s2, "3": s3, "4": s4})
-# data.boxplot()  # 这里，pandas自己有处理的过程，很方便哦。
-# plt.ylabel("ylabel")
-# plt.xlabel("xlabel")  # 我们设置横纵坐标的标题。
-# plt.show()
-
-# province_dict_df = pd.read_excel(ROOT_DIR + 'settings/province_badrate_classification.xlsx', encoding='utf-8')
-# y = 'loan_status'
-# x = set(list(province_dict_df)).difference(set([y]))
-# print(x)
-# #dict = province_dict_df.set_index('identity_province').T.to_dict('int')['classification']
-#
-# print(set(list(province_dict_df)))
-
-
-# condition_list = ['var == 0', 'var >=1 and var <= 2', 'var == 3']
-# s = 0
-# x = '2'
-# for condition in condition_list:
-#     condition = condition.replace("var", str(x))
-#     print(condition)
-#     if eval(condition):
-#         print('matched !')
-#         print(s)
-#     else:
-#         s += 1
-#
-# dicts = {'job_level': {'missing': 0.00014964459408903854, '一般工人': 0.0032921810699588477, '一般科员': 0.0017209128320239432, '专业技术人员': 0.016610549943883276, '专员': 0.00014964459408903854, '主任/主管/组长/初级管理': 0.2680882903105125, '保安': 0.0006734006734006734, '其他': 0.00202020202020202, '军人': 0.00014964459408903854, '农林牧渔养殖人员': 0.00014964459408903854, '助理': 0.00014964459408903854, '医护人员': 0.0014964459408903852, '厨师': 0.0023943135054246166, '司机': 0.002244668911335578, '学生': 0.048484848484848485, '实习生': 0.0010475121586232697, '工程师': 0.002543958099513655, '总监/总经理/高管': 0.029405162738496072, '技术工人': 0.012719790497568275, '教师': 0.001421623643845866, '普通员工': 0.39206883651328095, '服务员': 0.001421623643845866, '法人/老板': 0.00935278713056491, '演员/运动员': 7.482229704451927e-05, '科级以上': 0.0003741114852225963, '经理/中级管理': 0.17949869060980173, '行政/人力资源': 0.005462027684249906, '设计师': 0.0008230452674897119, '财会人员': 0.00404040404040404, '销售人员': 0.011971567527123082}}
-# print()
-
-# condition_list = ['var == 0', 'var >=1 and var <= 2', 'var == 3']
-# s = 0
-# x = '2'
-# for condition in condition_list:
-#     condition = condition.replace("var", str(x))
-#     print(condition)
-#     if eval(condition):
-#         print('matched !')
-#         print(s)
-#     else:
-#         s += 1
-#
-# WOE_IV_dict = {}
-# WOE_IV_dict1 = {}
-# var = 'zhima_score_classification'
-# new_var = var + '_WOE'
-# train_data = pd.read_excel(ROOT_DIR + 'transformed_train.xlsx', encoding = 'utf-8')
-#
-# merged_dict = MergeBad0(train_data, var, LABEL, direction='bad')
-# train_data[var] = train_data[var].map(lambda x : merged_dict[x])
-# print(merged_dict)
-# print(set(train_data[var]))
-#
-# WOE_IV_dict[new_var] = CalcWOE(train_data, var, LABEL)
-# train_data[new_var] = train_data[var].map(lambda x : WOE_IV_dict[new_var]['WOE'][x])
-#
-# print(WOE_IV_dict[new_var])
-#
-# print('****************************************')
-#
-# merged_dict1 = MergeBad0(train_data, var, LABEL, direction='good')
-# train_data[var] = train_data[var].map(lambda x : merged_dict1[x])
-# print(merged_dict1)
-# print(set(train_data[var]))
-#
-# WOE_IV_dict1[new_var] = CalcWOE(train_data, var, LABEL)
-# train_data[new_var] = train_data[var].map(lambda x : WOE_IV_dict1[new_var]['WOE'][x])
-#
-# print(WOE_IV_dict1[new_var])
-
-
-with open(ROOT_DIR + 'featureEngineering/featuresInModel.pkl', 'rb') as f:
-    featuresInModel = pickle.load(f)
-
-df = pd.read_excel('D:/conf_test/train_WOE_data1.xlsx')[featuresInModel + ['user_id', LABEL]]
-#df2 = pd.read_excel('D:/conf_test/train_WOE_data2.xlsx')[featuresInModel + ['user_id', LABEL]]
-
-
-regroup = BinBadRate(df, 'job_level', LABEL)[1]
-regroup_bad = regroup.sort_values(by  = 'bad_rate')
-regroup_good = regroup.sort_values(by='bad_rate',ascending=False)
-
-print(regroup_bad)
-print('****************')
-print(regroup_good)
